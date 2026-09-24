9591c3d165117352931bff2bbe503348653ef77d
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2026-09-24 18:13:58 -0400

    fix: revert back to class decorator

--- ape_sonic/ecosystem.py
+++ ape_sonic/ecosystem.py
@@ -30,7 +30,7 @@ class Block(EthereumBlock):
     timestamp_nano: HexInt = Field(alias="timestampNano")
 
     @model_validator(mode="before")
-    def validate_nano_timestamp(self, values: dict):
+    def validate_nano_timestamp(cls, values: dict):
         if not values.get("timestamp_nano"):
             # NOTE: For instances like using it with anvil or hardhat, which doesn't have this field
             values["timestamp_nano"] = values["timestamp"] * int(1e9)
